2c246056010c56a034547aad4b3ff9236449c74b
Kolya <k96260831@gmail.com>
2024-07-03 18:58:35 +0300

    login and register ui

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -8,8 +8,104 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  bool isObscureText = true;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Text(
+                  "Login",
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 1.4,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.email,
+                            size: 28,
+                          ),
+                          labelText: "email",
+                        ),
+                        style: Theme.of(context).textTheme.labelMedium,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Filed must not be empty";
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 14),
+                      TextFormField(
+                        obscureText: isObscureText,
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(
+                            Icons.lock,
+                            size: 28,
+                          ),
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isObscureText = !isObscureText;
+                              });
+                            },
+                            icon: isObscureText
+                                ? const Icon(Icons.visibility_off)
+                                : const Icon(Icons.visibility),
+                          ),
+                          labelText: "password",
+                        ),
+                        style: Theme.of(context).textTheme.labelMedium,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Filed must not be empty";
+                          } else if (value.length < 6) {
+                            return "Password must not be less then 6";
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {}
+                  },
+                  child: const Text("Login"),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account yet?",
+                      style: Theme.of(context).textTheme.titleSmall,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/registerPage");
+                      },
+                      child: const Text("Register"),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
